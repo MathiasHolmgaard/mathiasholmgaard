b81d38c35fa49effbb43d75322499303ada31080
Mathias Holmgaard <mathias.holmgaard@hotmail.com>
2024-02-10 11:58:19 +0100

    FirstCommit

---
+++ MathiasHolmgaard/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MathiasHolmgaard.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
